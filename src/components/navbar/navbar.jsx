0161d930f1e40f98998d232493f072169a1b7358
ridhwan <ridmartrite@gmail.com>
2023-08-22 18:04:18 +0100

    creating navbar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed top-0 left-0 right-0 bg-[#282C33] '>
+        <p>naffv</p>
+        <p>nav</p>
+    </div>
+  )
+}
+
+export default Navbar
