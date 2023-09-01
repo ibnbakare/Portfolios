5d2f09250c398da5a39791d4fdab7acdd366f5dc
ridhwan <ridmartrite@gmail.com>
2023-09-01 18:52:56 +0100

    starting project section

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,9 +1,13 @@
 import About from '@/components/about_page_section/About'
+import Project from '@/components/about_project_page/Project'
 import React from 'react'
 
 const Home = () => {
   return (
+    <>
     <About />
+    <Project />
+    </>
   )
 }
 
---
+++ src/components/about_project_page/Project.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Project = () => {
+    return (
+        <>
+            <div className='px-5 py-10   max-w-[1560px] h-min-screen bg-slate-500'>
+
+                {/* top */}
+                <div className="flex justify-between items-center">
+                    {/* left */}
+                    <div className="text-white font-medium text-[32px] w-2/3 flex items-center  gap-2">
+                        <div className=" ">
+                            <span className='text-[#C778DD]'>#</span>Project
+                        </div>
+                        <div className=" w-2/3 h-px bg-[#C77800] ">
+                          
+                        </div>
+
+                    </div>
+                    {/* right */}
+                    <div className="">right</div>
+                </div>
+                {/* bottom */}
+
+            </div>
+        </>
+    )
+}
+
+export default Project
