8739d3ec2512704460994ccac1a9a6ffe465defc
Ridhwan Bakare <ridmartrite@gmail.com>
2023-09-15 19:16:53 +0100

    done with project card

--- src/components/about_project_page/Project.jsx
+++ src/components/about_project_page/Project.jsx
@@ -1,10 +1,13 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import first from "./img/first.png"
+import second from "./img/second.jpg";
+import third from "./img/third.png";
+import Project_card from "../project_card/Project_card";
 
 const Project = () => {
-
-// date
-const projects = [
+  // date
+  const projects = [
     {
       img: first,
       langs: ["html", "css", "javascript", "react", "tailwind css"],
@@ -25,48 +28,44 @@ const projects = [
     },
   ];
 
-    return (
-        <>
-            <div className='px-5 py-10   max-w-[1560px] h-min-screen bg-slate-500 '>
-
-                {/* top */}
-                <div className="flex justify-between items-center mx-auto">
-                    {/* left */}
-                    <div className="text-white font-medium text-[32px] w-2/3 flex items-center  gap-2">
-                        <div className=" ">
-                            <span className='text-[#C778DD]'>#</span>Project
-                        </div>
-                        <div className=" w-full h-px bg-[#C778DD] ">
-                          
-                        </div>
-
-                    </div>
-                    {/* right */}
-                    <div className="text-white font-medium">
-                            <Link href="" > 
-                            <span>
-                                View all ~~&gt;
-                            </span>
-                            </Link>
-                    </div>
-                </div>
-                {/* bottom */}
-                <div className='flex flex-wrap justify-between gap-4 my-12'>
-                    {/* cards */}
-                    <>
+  return (
+    <>
+      <div className="px-5 py-10   max-w-[1560px] h-min-screen bg-[#282C33] ">
+        {/* top */}
+        <div className="flex justify-between items-center mx-auto">
+          {/* left */}
+          <div className="text-white font-medium text-[32px] w-2/3 flex items-center  gap-2">
+            <div className=" ">
+              <span className="text-[#C778DD]">#</span>Project
+            </div>
+            <div className=" w-full h-px bg-[#C778DD] "></div>
+          </div>
+          {/* right */}
+          <div className="text-white font-medium">
+            <Link href="">
+              <span>View all ~~&gt;</span>
+            </Link>
+          </div>
+        </div>
+        {/* bottom */}
+        <div className="flex flex-wrap justify-between gap-4 my-12">
+          {/* cards */}
+          {projects.map(({ img, langs, title, disc }) => {
+            return (
+              <>
                 <Project_card
                   img={img}
                   langs={langs}
                   title={title}
                   disc={disc}
                 />
-                    </>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 
-                </div>
-
-            </div>
-        </>
-    )
-}
-
-export default Project
+export default Project;
--- src/components/project_card/Project_card.jsx
+++ src/components/project_card/Project_card.jsx
@@ -1,12 +1,15 @@
+import Image from "next/image";
 import React from "react";
 
-const Project_card = ({ img, langs, title }) => {
+const Project_card = ({ img, langs,disc, title }) => {
   return (
     <>
       <div className="border mx-auto w-[90%] md:w-[48%] lg:w-[30%] border-[#ABB2BF] p-0">
         {/* img */}
         <div className="">
-          <img className=" w-full" src={img} alt="" />
+        {/* <Image src={img} alt="" width={"15rem"} height={"7.6rem"} /> */}
+        <Image src={img} width={457} height={386} className="contain" />
+          {/* <Image className=" w-full" src={img} alt="" /> */}
         </div>
         {/* Skills */}
         <div className="flex gap-2 flex-wrap p-2 border-y border-[#ABB2BF]">
@@ -15,7 +18,17 @@ const Project_card = ({ img, langs, title }) => {
           })}
         </div>
         {/* disc */}
-      </div>
+        <div className=" p-4">
+          <h2 className="text-[#FFFFFF] text-2xl font-medium">{title}</h2>
+          <p className=" py-4 text-[#ABB2BF]">{disc}</p>
+        </div>
+        {/* btns */}
+        <div className="">
+          <button className=" py-2 px-4 text-white border border-[#C778DD] hover:bg-[#C778DD33] duration-150">
+            Live {"<"}~{">"}
+          </button>
+        </div>
+     </div>
     </>
   );
 };
